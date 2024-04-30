6fbd1adf7170b13962014472ab156658090bd5fc
lucarin91 <lucarin@protonmail.com>
2024-04-30 12:41:53 +0200

    add golang plugin

--- init.lua
+++ init.lua
@@ -883,6 +883,20 @@ require('lazy').setup({
     end,
   },
   'github/copilot.vim',
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
